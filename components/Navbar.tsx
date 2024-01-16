c74f2faedc04cca75e3f932a4b7bd328beb3227a
Ayman Mustafa <mswain6@saddleback.edu>
2024-01-16 23:58:55 +0200

    Session 11 P1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,13 @@ import { Button } from "./ui/button";
 import { Menu, icons } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
+import { getLimitCount } from "@/lib/use-limit";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const LimitCount = await getLimitCount()
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar LimitCount={LimitCount} />
       <div className="flex w-full justify-end">
         <Button variant={"ghost"} className="mr-2">
           Arabic
--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,7 +5,10 @@ import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./Sidebar";
 
-const MobileSidebar = () => {
+interface MobileSidebarProps {
+  LimitCount: number;
+}
+const MobileSidebar = ({ LimitCount }: MobileSidebarProps) => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(() => {
     setIsMounted(true);
@@ -21,7 +24,7 @@ const MobileSidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side={"left"} className="p-0">
-        <Sidebar />
+        <Sidebar limitCount={LimitCount} />
       </SheetContent>
     </Sheet>
   );
