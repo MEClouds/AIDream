6a387570d4b089b3971d5d8b11fe598fdacc2b9b
Ayman Mustafa <84941140+MEClouds@users.noreply.github.com>
2024-01-24 20:08:09 +0200

    update prismadb to work on the Edge

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,9 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
 
-export default prismadb;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
